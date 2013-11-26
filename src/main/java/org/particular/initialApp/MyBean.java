e50db5f785661ac05c81b81a869989f881cd85d7
rlbatista <sonomel00@yahoo.com.br>
2013-11-26 14:29:56 -0200

    Manuseio do mapa por javascript e model do primefaces

--- src/main/java/org/particular/initialApp/MyBean.java
+++ src/main/java/org/particular/initialApp/MyBean.java
@@ -3,16 +3,26 @@ package org.particular.initialApp;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 
+import org.primefaces.model.map.DefaultMapModel;
+import org.primefaces.model.map.LatLng;
+import org.primefaces.model.map.MapModel;
+import org.primefaces.model.map.Marker;
+
 @ManagedBean(name = "myBean")
-@SessionScoped
+@ViewScoped
 public class MyBean implements Serializable {
 
     private static final long serialVersionUID = 8301865434469950945L;
 
     String str = "hello";
+    
+    private MapModel simpleModel = new DefaultMapModel();
+    
+    private double latitude;
+    private double longitude;
 
     public String getStr() {
         return str;
@@ -26,4 +36,34 @@ public class MyBean implements Serializable {
         str = "";
     }
 
+    public MapModel getSimpleModel() {
+		return simpleModel;
+	}
+    
+    public double getLatitude() {
+		return latitude;
+	}
+    
+    public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+    
+    public double getLongitude() {
+		return longitude;
+	}
+    
+    public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+    
+    public void adicionaMarcacao() {
+    	LatLng coordenada = new LatLng(latitude, longitude);
+    	
+    	simpleModel.addOverlay(new Marker(coordenada, "Coordenada Informada"));
+    }
+    
+    public void enderecoDeCasa() {
+    	latitude = -22.820492;
+    	longitude = -47.265501;
+    }
 }
